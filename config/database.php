e54b9bd3bf3c38d2c6399884736e5316d21f7172
Boris Budini <Boris@budini.me>
2018-02-16 16:20:24 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -52,6 +52,18 @@ return [
             'strict' => true,
             'engine' => null,
         ],
+        
+          'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
+            'username'  => env('DB_TEST_USERNAME', 'homestead'),
+            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
