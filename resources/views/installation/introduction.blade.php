23a631b7ac69eb6f605816d72258a328c606b2fd
Boris Budini <Boris@budini.me>
2018-03-03 16:52:07 +0100

    Replaced lorem ipsum

--- resources/views/installation/introduction.blade.php
+++ resources/views/installation/introduction.blade.php
@@ -6,11 +6,11 @@
             <div class="template-logo-description">
                 <header>
                     <img width="75" height="75" src="/images/logo.svg">
-                    <h2>Installation Process</h2>
+                    <h2>Welcome to Identihub</h2>
                 </header>
                 <section>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                    <p>Welcome to Identihub.</p>
+                    <p>The open source software developed by Ura to help you host your brand.</p>
                     <a class="button" href="{{ route('introduction.database') }}">Continue</a>
                 </section>
             </div>
