327b66817bf0620f898e5e855bf078d982136861
Boris Budini <Boris@budini.me>
2018-03-23 15:16:41 -0500

    Revert "The Svg is now concealed"

--- app/Models/Icon.php
+++ app/Models/Icon.php
@@ -12,36 +12,33 @@ class Icon extends Model
     protected $table = 'icons';
 
     protected $fillable = [
-        'filename', 'filename_png', 'bridge_id', 'width_ratio', 'section_id', 'order'
+        'filename', 'bridge_id', 'width_ratio', 'section_id', 'order'
     ];
-
-    public function getFilenamePngAttribute($value)
-    {
-        if ($value == null) {
-            return $this->filename;
-        }
-    }
 
     public function converted()
     {
         return $this->hasMany(IconConverted::class, 'icon_id', 'id');
     }
 
-    public function save(array $options = [])
-    {
-        // before save code
+	public function save(array $options = [])
+	{
+		// before save code
 
-        parent::save();
-        // after save code
-        Bridge::whereId($this->bridge_id)->increment('nr_icons');
-    }
+		parent::save();
+		// after save code
+		Bridge::whereId($this->bridge_id)->increment('nr_icons');
 
-    public function update(array $attributes = [], array $options = [])
-    {
-        // before update code
 
-        parent::update();
-        // after update code
-        Bridge::whereId($this->bridge_id)->increment('nr_icons');
-    }
+	}
+
+	public function update(array $attributes = [], array $options = [])
+	{
+		// before update code
+
+		parent::update();
+		// after update code
+		Bridge::whereId($this->bridge_id)->increment('nr_icons');
+
+	}
+
 }
