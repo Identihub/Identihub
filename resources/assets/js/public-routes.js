f2b8dd259a3bab9a99317081ac9c8cc607faf923
Alban Afmeti <albanafmeti@gmail.com>
2018-07-14 12:40:03 +0200

    fixed little bug

--- resources/assets/js/public-routes.js
+++ resources/assets/js/public-routes.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import {HashRouter, Route} from 'react-router-dom';
 import Bridge from './pages/Bridge';
 import Helmet from 'react-helmet';
-import Viewer from './pages/Viewer';
+import Viewer from './pages/viewer/Viewer';
 
 export default (
     <div>
         <HashRouter>
             <div>
                 <Helmet
-                    titleTemplate="Identihub - %s"
-                >
+                    titleTemplate="Identihub - %s">
                 </Helmet>
                 <Route path="/" component={Bridge}/>
                 <Route path="/view/:objectType/element/:elementId" component={Viewer}/>
