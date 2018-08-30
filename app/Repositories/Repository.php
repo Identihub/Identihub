4c5317ccd96f620b3f61a72dda848cd239a7b437
Alban Afmeti <albanafmeti@gmail.com>
2018-08-31 00:10:53 +0200

    manipulating svg #inprogress

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -16,14 +16,32 @@ class Repository
     protected function addViewBoxAndScaleSvg($svgData, $width, $height)
     {
         $xml = simplexml_load_string($svgData);
+
         $attr = $xml->attributes();
 
         if (strpos($svgData, "viewBox") == false) {
-            $xml->addAttribute('viewBox', "0 0 ".$attr->width." ".$attr->height);
+            $xml->addAttribute('viewBox', "0 0 " . $width . " " . $height);
+        } else {
+            $xml->attributes()->viewBox = "0 0 " . $width . " " . $height;
         }
 
-        $xml->attributes()->width = $width;
-        $xml->attributes()->height = $height;
+        if (strpos($svgData, "style") == false) {
+            $xml->addAttribute('style', "enable-background:new 0 0 $width $height;");
+        } else {
+            $xml->attributes()->style = "enable-background:new 0 0 $width $height;";
+        }
+
+        if (strpos($svgData, "width") == false) {
+            $xml->addAttribute('width', $width);
+        } else {
+            $xml->attributes()->width = $width;
+        }
+
+        if (strpos($svgData, "height") == false) {
+            $xml->addAttribute('height', $height);
+        } else {
+            $xml->attributes()->height = $height;
+        }
 
         return $xml->asXML();
     }
