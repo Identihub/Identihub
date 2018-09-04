446fb726091fd7a238ec778e1b6fd927061a96c2
Alban Afmeti <albanafmeti@gmail.com>
2018-09-04 22:00:35 +0200

    added fixes

--- app/Jobs/CreateFontImage.php
+++ app/Jobs/CreateFontImage.php
@@ -55,9 +55,9 @@ class CreateFontImage implements ShouldQueue
         $drawOther->setFont($filePath);
         $drawOther->setStrokeWidth(1);
         $drawOther->setFontSize(25);
-        $drawOther->annotation(155, 70, "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z");
-        $drawOther->annotation(155, 130, "a b c d e f g h i j k l m n o p q r s t u v w x y z");
-        $drawOther->annotation(155, 190, "1 2 3 4 5 6 7 8 9 0 ' ? ` \" !");
+        $drawOther->annotation(170, 70, "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z");
+        $drawOther->annotation(170, 130, "a b c d e f g h i j k l m n o p q r s t u v w x y z");
+        $drawOther->annotation(170, 190, "1 2 3 4 5 6 7 8 9 0 ' ? ` \" !");
 
         $imagick = new \Imagick();
         $imagick->newImage(800, 240, 'transparent');
